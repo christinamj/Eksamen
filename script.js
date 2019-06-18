b4ccdffe600367689a957e016202a41f1de19a95
Christina Jørgensen <christina@desktop-jcrl3a0.kea.local>
2019-06-18 22:19:41 +0200

    test

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+    document.querySelector("#menuknapluk").addEventListener("click", toggleMenu);
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#burger").classList.toggle("hidden");
+
+    if (document.querySelector("#containerforside")) {
+        document.querySelector("#containerforside").classList.toggle("hidden");
+    }
+
+    if (document.querySelector("#menuknap")) {
+        document.querySelector("#menuknap").classList.toggle("hidden");
+    }
+
+    if (document.querySelector("#portfoliecontainer")) {
+        document.querySelector("#portfoliecontainer").classList.toggle("hidden");
+    }
+
+    if (document.querySelector("#dokcontainer")) {
+        document.querySelector("#dokcontainer").classList.toggle("hidden");
+    }
+
+    if (document.querySelector("#omcontainer")) {
+        document.querySelector("#omcontainer").classList.toggle("hidden");
+    }
+
+
+    if (document.querySelector("#colom2")) {
+        document.querySelector("#colom2").classList.toggle("hidden");
+    }
+}
